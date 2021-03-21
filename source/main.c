3921c8ba9aa6e645dadedb84b2f8c5f1b20628a8
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2021-03-21 22:28:03 +0100

    PIR driver and interrupt setup

--- source/main.c
+++ source/main.c
@@ -5,7 +5,7 @@
 #include "thread.h"
 #include "shell.h"
 #include "led.h"
-//#include "pir.h"
+#include "pir.h"
 
 #include "periph/gpio.h"
 
@@ -16,13 +16,13 @@
 #include "hd44780.h"
 #include "thread.h"
 
-//char rcv_thread_stack[THREAD_STACKSIZE_MAIN];
+char pir_handler_stack[THREAD_STACKSIZE_MAIN];
 
 dht_t dht_dev;
+pir_t pir_dev;
+hd44780_t display_dev;
 int th[2];
 char msg[32];
-
-
 
 
     
@@ -42,8 +42,99 @@ void dht_temp_read(void) {
 /*lcd*/
 int lcd_write(char msg[]){
     
-    hd44780_t dev;
-    hd44780_params_t params = {
+    // clear screen, reset cursor 
+   
+    hd44780_clear(&display_dev);
+    hd44780_home(&display_dev);
+    // write first line 
+    hd44780_print(&display_dev,msg);
+    
+    /* set cursor to second line and write */
+    hd44780_set_cursor(&display_dev, 0, 1);
+    hd44780_print(&display_dev, "Ciao");
+
+    return 0;
+}
+
+
+/* main interrupt handler */
+
+
+void* pir_handler(void *arg)
+{
+    (void)arg;
+    msg_t msg_q[1];
+    msg_init_queue(msg_q, 1);
+
+    printf("Registering PIR handler thread...     %s\n",
+           pir_register_thread(&pir_dev) == 0 ? "[OK]" : "[Failed]");
+
+    msg_t m;
+    while (msg_receive(&m)) {
+        printf("PIR handler got a message: ");
+        switch (m.type) {
+            case PIR_STATUS_ACTIVE: {
+                puts("something started moving.\n");
+                gpio_write(GPIO_PIN(PORT_B,5),1);
+                dht_temp_read();
+                sprintf(msg,"T %d H %d%%",th[0],th[1]);
+                printf("msg: %s\n",msg);
+                lcd_write(msg);
+                break;
+            }
+            case PIR_STATUS_INACTIVE:{
+                puts("the movement has ceased.");
+                gpio_write(GPIO_PIN(PORT_B,5),0);
+                break;
+            }
+            default:
+                puts("stray message.");
+                break;
+        }
+    }
+    puts("PIR handler: this should not have happened!");
+
+    return NULL;
+}
+
+int main(void){
+    
+    printf("[Start SETUP]\n");
+    
+    // init relay pin
+    gpio_init(GPIO_PIN(PORT_B, 5),GPIO_OUT);
+    
+    // init pir sensor
+    pir_params_t pir_params = {0};
+    pir_params.gpio = GPIO_PIN(PORT_A,10);
+    pir_params.active_high=1;
+    if (pir_init(&pir_dev, &pir_params) == 0) {
+        puts("[PIR OK]\n");
+    }
+    else {
+        puts("[Failed PIR]");
+        return 1;
+    }
+    
+     
+    // init dht  
+    dht_params_t dht_params ={0};
+    
+    dht_params.pin=GPIO_PIN(PORT_B, 3);
+    dht_params.type=DHT11;
+    dht_params.in_mode=GPIO_IN;
+    
+     if (dht_init(&dht_dev,&dht_params) == 0) {
+        puts("[DHT OK]\n");
+    }
+    else {
+        puts("[Failed DHT]");
+        return 1;
+    }
+    
+    // init display 
+    
+     hd44780_params_t display_params = {
     
     
         .cols=(16U),
@@ -56,84 +147,20 @@ int lcd_write(char msg[]){
                GPIO_PIN(PORT_A, 5),GPIO_UNDEF,GPIO_UNDEF,GPIO_UNDEF,GPIO_UNDEF}
     };
     
-    // init display 
-    //puts("[START]");
-    if (hd44780_init(&dev, &params) != 0) {
-        puts("[FAILED]");
+    if (hd44780_init(&display_dev, &display_params) == 0) {
+        puts("[DISPLAY OK]");
+    }else{
+        puts("[Failed DISPLAY]");
         return 1;
     }
-    // clear screen, reset cursor 
-   
-    hd44780_clear(&dev);
-    hd44780_home(&dev);
-    // write first line 
-    hd44780_print(&dev,msg);
-
-    return 0;
-}
-/*
-void *rcv_thread(void *arg)
-{
-    (void) arg;
-    //sprintf(msg,"T %d H %d%%",th[0],th[1]);
-    //printf("msg: %s\n",msg);
-    lcd_write("Ciao");
-    return NULL;
     
-}
-*/
-/* main interrupt handler */
-void cb_f(void* args){
+    //init pir interrupt routine
+    thread_create(
+           pir_handler_stack, sizeof(pir_handler_stack), THREAD_PRIORITY_MAIN - 1,
+           THREAD_CREATE_WOUT_YIELD | THREAD_CREATE_STACKTEST,
+           pir_handler, NULL, "pir_handler");
     
-    (void) args;
-    
-    
-    printf("MVNT detected \n");
-    
-    printf("LGHT on \n");
-    
-    gpio_write(GPIO_PIN(PORT_B,5),1);
-    
-    dht_temp_read();
-    
-    printf("T %d H %d%%\n",th[0],th[1]);
-    
-    /*thread_create(rcv_thread_stack, sizeof(rcv_thread_stack),
-                  THREAD_PRIORITY_MAIN-1, THREAD_CREATE_STACKTEST,
-                  rcv_thread, NULL, "rcv_thread");*/
-    
-    lcd_write("Ciao");
-
-    xtimer_sleep(5);
-    
-    gpio_write(GPIO_PIN(PORT_B,5),0);
-        
-    printf("LGHT off \n");
-    
-}
-
-int main(void){
-    
-    printf("Start SETUP...\n");
-    
-    // init pir pin
-    gpio_init(GPIO_PIN(PORT_B, 5),GPIO_OUT);
-    
-     
-    // init dht  
-    dht_params_t params ={0};
-    
-    params.pin=GPIO_PIN(PORT_B, 3);
-    params.type=DHT11;
-    params.in_mode=GPIO_IN;
-    
-    dht_init(&dht_dev,&params);	
-    
-    
-    //init pir int
-    gpio_init_int(GPIO_PIN(PORT_A, 10),GPIO_IN,GPIO_RISING,cb_f,NULL);
-    
-    printf("SETUP done!\n");
+    printf("[SETUP done]!\n");
     
     return 0;
 }
